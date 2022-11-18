c27608bc5c65cc5be8a2c3a7ebfefb87c713fb54
Oroch <oliveirarocha_pedro@hotmail.com>
2022-11-18 20:36:51 -0300

    CRUD ANGULAR FINALIZADO

---
+++ aula12/crud-rest/src/app/views/cliente/cliente.component.ts
@@ -0,0 +1,82 @@
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from 'src/app/Model/cliente';
+import { ClienteService } from 'src/app/service/cliente.service';
+
+@Component({
+  selector: 'app-cliente',
+  templateUrl: './cliente.component.html',
+  styleUrls: ['./cliente.component.css']
+})
+export class ClienteComponent implements OnInit {
+
+  listaClientes: Cliente[] = [];
+  cliente?: Cliente;
+  isEditn = false;
+
+  constructor(private clienteService: ClienteService) { }
+
+  ngOnInit(): void {
+    this.listar();
+  }
+
+  listar()
+  {
+    this.clienteService.listar().subscribe(clientes => {
+      this.listaClientes = clientes;
+    });
+  }
+
+  novo()
+  {
+    this.cliente = new Cliente();
+    this.isEditn = false;
+  }
+
+  cancelar()
+  {
+    this.cliente = undefined;
+  }
+
+  salvar()
+  {
+
+    if (!this.cliente) {
+      return;
+    }
+
+    if(!this.isEditn) {
+      this.clienteService.inserir(this.cliente). subscribe(cliente => {
+        this.listar();
+        this.cancelar();
+      });
+    } else {
+      this.clienteService.atualizar(this.cliente). subscribe(cliente => {
+        this.listar();
+        this.cancelar();
+      });
+    }
+
+
+  }
+
+  excluir(id?: number) {
+
+    if(!id) {
+      return;
+    }
+
+    const resposta = confirm('Esse cliente será excluído ok?');
+
+    if (resposta) {
+      this.clienteService.excluir(id).subscribe(( ) => {
+        this.listar();
+      });
+    }
+  }
+
+  selecionar(cliente: Cliente) {
+    this.cliente = cliente;
+    this.isEditn = true;
+  }
+
+}
