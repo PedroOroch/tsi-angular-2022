cd0d131c3930ba197668fdfe7a3cf86ba4bc77bd
Oroch <oliveirarocha_pedro@hotmail.com>
2022-11-18 22:04:09 -0300

    CRUD FINALIZADO E ESTILIZANDO A PÁGINA CRUD

--- aula12/crud-rest/src/app/app.module.ts
+++ aula12/crud-rest/src/app/app.module.ts
@@ -5,6 +5,13 @@ import { HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClienteComponent } from './views/cliente/cliente.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule,
+              MatIconModule,
+              MatListModule,
+              MatSidenavModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
@@ -15,7 +22,13 @@ import { ClienteComponent } from './views/cliente/cliente.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    BrowserAnimationsModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
